0ef8af3b845127ceade708cc453eb7e59389f9a0
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-07-11 00:12:30 -0400

    Update linearalgebra_types.jl

--- src/linearalgebra_types.jl
+++ src/linearalgebra_types.jl
@@ -6,3 +6,12 @@ for O in (:BunchKaufman, :GeneralizedSVD, :Schur)
     @eval LinearAlgebra.$O(::Type{T}) where {T} = LinearAlgebra.$O{T, Array{T,2}}
 end
 
+
+for (O,F) in ( (:Adjoint, :adjoint), (:Transpose, :transpose),
+               (:BunchKaufman, :bunchkaufman), (:Cholesky, :cholseky), (:Hessenberg, :hessenberg),
+               (:GeneralizedSVD, :svd), (:LDLt, :ldlt), (:LQ, :lq), (:LU, :lu), (:QR, :qr), (:Schur, :schur))
+    @eval begin
+        LinearAlgebra.$F(x::A) where {N, T<:AbstractFinite, A<:AbstractArray{T,N}} =
+            T.(LinearAlgebra.$F(float(T).(x)))     
+    end
+end
